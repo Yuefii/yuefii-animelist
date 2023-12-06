d5c191cdeee95f9d8d12f93af0c54de8bf06a4f0
yuefii <muffidahmadiawan31@gmail.com>
2023-12-06 08:35:34 +0700

    feat: recommendations anime

--- app/libs/api.ts
+++ app/libs/api.ts
@@ -4,3 +4,18 @@ export const getAnimeResponse = async (resource: string, query?: string) => {
   const anime = await result.json();
   return anime
 }
+
+export const getNestedAnimeResponse = async (resource: string, objectProperty: number) => {
+  const result = await getAnimeResponse(resource)
+  return result.data.flatMap((item: string) => item[objectProperty])
+}
+
+export const reproduce = (data: string | any[], gap: number) => {
+  const first = ~~(Math.random() * (data.length - gap) + 1)
+  const last = first + gap
+
+  const result = {
+    data: data.slice(first, last)
+  }
+  return result
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 import TopAnime from "@/app/components/TopAnime";
 import Header from "@/app/components/TopAnime/header";
-import { getAnimeResponse } from "@/app/libs/api";
+import {
+  getAnimeResponse,
+  getNestedAnimeResponse,
+  reproduce,
+} from "@/app/libs/api";
 
 const Page = async () => {
   const topAnime = await getAnimeResponse("top/anime", "limit=10");
+  let recomendations = await getNestedAnimeResponse(
+    "recommendations/anime",
+    "entry"
+  );
+  recomendations = reproduce(recomendations, 5);
   return (
     <>
       <div className="container mx-auto">
+        <section>
+          <Header title="Recomendations" />
+          <TopAnime topAnime={recomendations} />
+        </section>
         <section>
           <Header title="Top Anime" linkTitle="See All" linkHref="/top-anime" />
           <TopAnime topAnime={topAnime} />
